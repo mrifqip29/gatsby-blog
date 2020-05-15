75ff498e64715633675e09d98059bdd409cc29b3
waryuu <01wahyudiputra01@gmail.com>
2020-05-16 05:59:02 +0700

    add paginator things

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,22 +1,48 @@
+
+
 const path = require("path")
+const { paginate } = require('gatsby-awesome-pagination');
 
 module.exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
-  const blogTemplate = path.resolve("./src/templates/kabarterkini.js")
+  //     createItems({
+  //       path:`page/${currentPage}`,
+  //       component: path.resolve(`src/templates/blog.js`)
+  //     })
+  // }
 
-  const res = await graphql(`
+  const blogTemplate = path.resolve("./src/templates/kabarterkini.js")
+  const MAX_POST = 2
+
+  const result = await graphql(`
     query {
       allContentfulBlogPost {
         edges {
           node {
+            id
             slug
           }
         }
       }
     }
   `)
-  res.data.allContentfulBlogPost.edges.forEach(edge => {
+
+  if (result.errors) {
+    console.error(result.errors)
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
+  }
+
+  paginate({
+    createPage,
+    items: result.data.allContentfulBlogPost.edges,
+    itemsPerPage: MAX_POST,
+    pathPrefix : '/kabarterkini',
+    component: path.resolve(`./src/pages/kabarterkini.js`),
+  });
+
+  result.data.allContentfulBlogPost.edges.forEach(edge => {
     createPage({
       component: blogTemplate,
       path: `/kabarterkini/${edge.node.slug}`,
