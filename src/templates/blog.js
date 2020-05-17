5674d953c3eb58c864b518bb51297d6104ea31d1
Muhammad Rifqi Prabantoro <45098478+mrifqip29@users.noreply.github.com>
2020-05-17 12:32:44 +0700

    fixing cardkabar.js and blog.js (templates)

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link, graphql} from "gatsby"
+import { Link, graphql } from "gatsby"
 import SEO from "../components/seo"
 import PropTypes from "prop-types"
 import Layout from "../components/layout"
@@ -8,56 +8,56 @@ import blogStyles from "../pages/blog.module.scss"
 import Pager from "../components/paginator"
 import Kategori from "../components/kategori"
 
+const Blog = ({ data, pageContext }) => {
+  return (
+    <Layout>
+      <SEO title="Blog Posts" />
+      <div className="h1 text-center"> Kabar Terkini</div>
+      <ol className={blogStyles.posts}>
+        {data.allContentfulBlogPost.edges.map(edge => {
+          return (
+            <li className={blogStyles.post}>
+              <Link to={`/kabarterkini/${edge.node.slug}`}>
+                <h2>{edge.node.title}</h2>
+                <p>{edge.node.publishedDate}</p>
+              </Link>
+            </li>
+          )
+        })}
+      </ol>
+      <Pager pageContext={pageContext} />
 
-const Blog =  ({data, pageContext}) => {
-    return (
-      <Layout>
-        <SEO title= "Blog Posts" />
-        <div className="h1 text-center"> Kabar Terkini</div>
-        <ol className={blogStyles.posts}>
-          {data.allContentfulBlogPost.edges.map(edge => {
-            return (
-              <li className={blogStyles.post}>
-                <Link to={`/kabarterkini/${edge.node.slug}`}>
-                  <h2>{edge.node.title}</h2>
-                  <p>{edge.node.publishedDate}</p>
-                </Link>
-              </li>
-            )
-          })}
-        </ol>
-        <Pager pageContext= {pageContext} />
-
-        <Kategori/>
-      </Layout>
-    )
-  }
+      <Kategori />
+    </Layout>
+  )
+}
 
 Blog.propTypes = {
   data: PropTypes.object.isRequired,
   pageContext: PropTypes.object.isRequired,
 }
 
-
-
 export const query = graphql`
-query ($skip: Int, $limit:Int) {
-  allContentfulBlogPost(
-    sort: {fields: title, order: DESC}
-    skip: $skip
-    limit: $limit
-    ){
-     edges {
-      node {
-        id
-        title
-        slug
-        publishedDate
-        gambarArtikel{
-         file{url}
+  query($skip: Int, $limit: Int) {
+    allContentfulBlogPost(
+      sort: { fields: title, order: DESC }
+      skip: $skip
+      limit: $limit
+    ) {
+      edges {
+        node {
+          id
+          title
+          slug
+          publishedDate(formatString: "MMMM Do, YYYY")
+          gambarArtikel {
+            file {
+              url
+            }
+          }
         }
       }
     }
   }
-}`
+`
 export default Blog
