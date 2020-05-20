473fbba0a07569f1a77b01eaafe566700928a3cf
waryuu <01wahyudiputra01@gmail.com>
2020-05-20 07:55:01 +0700

    copy blog ke tags-template

--- src/templates/tags-template.js
+++ src/templates/tags-template.js
@@ -15,44 +15,120 @@ const Tags = ({ data,pageContext }) => {
   const { totalCount } = data.allContentfulBlogPost;
   const tagHeader = `${totalCount} post${
     totalCount === 1 ? '' : 's'
-  } tagged with "${tag}"`;
+  }  berkategori "${tag}" :`;
   return (
     <Layout>
-            <Jumbo
-              title="KABAR TERKINI"
-              page="Halaman Kabar Terkini"
-              image={data.contentfulJumbotronHalaman.jumbotronGambar.fixed.srcWebp}
-            />
-            <div className="row">
-              <div className="col-md-9 col-lg-9">
-                <ol className={blogStyles.posts}>
-                  {data.allContentfulBlogPost.edges.map(edge => {
-                    return (
-                      // <div className="col-lg-3 col-md-6 col-sm-12 py-1">
-                      <div
-                        className="card"
-                        style={{ minHeight: "100px", marginBottom: "30px" }}
-                      >
-                        <div className="card-body text-center">
-                          <li className={blogStyles.post}>
-                            <Link to={`/kabarterkini/${edge.node.slug}`}>
-                              <h2>{edge.node.title}</h2>
-                              <p>{edge.node.publishedDate}</p>
-                            </Link>
-                          </li>
+          <Jumbo
+            title="KABAR TERKINI"
+            nav="Informasi"
+            page="Halaman Kabar Terkini"
+            image={data.contentfulJumbotronHalaman.jumbotronGambar.fixed.srcWebp}
+          />
+          <div className=" col-md-8 col-lg-9 text-center">
+            <h1 className=" px-5"> {tagHeader} </h1>
+          </div>
+          <div className="row text-center px-5 py-3">
+
+            <div className=" col-md-8 col-lg-9 ">
+              {data.allContentfulBlogPost.edges.map(edge => {
+                return edge.node.gambarArtikel === null ? (
+                  <Link to={`/kabarterkini/${edge.node.slug}`} style={{textDecoration:'none'}}>
+                  <div
+                    className="card"
+                    style={{
+                      borderRadius: "45px",
+                      minHeight: "100px",
+                      marginBottom: "30px",
+                    }}
+                  >
+                    <img
+                      className="card-img-top"
+                      src={
+                        data.contentfulJumbotronHalaman.jumbotronGambar.fixed
+                          .srcWebp
+                      }
+                      style={{
+                        height: "20rem",
+                        objectFit: "cover",
+                        borderRadius: "45px 45px 0px 0px",
+                      }}
+                    ></img>
+                    <div className="card-body">
+                      <div className="container">
+                        <div className="row">
+                          <div className="col-sm-12 col-md-8 col-lg-10 text-left pl-3 pb-3">
+                            <h4 style={ {color:'#005131'}}>{edge.node.title}</h4>
+                          </div>
+
+                          <div className="col-sm-12 col-md-4 pl-0 col-lg-2 text-left mb-3">
+                            {edge.node.publishedDate}
+                          </div>
                         </div>
+
                       </div>
-                    )
-                  })}
-                </ol>
-                <Pager pageContext={pageContext} />
-              </div>
-              <div className="col-md-3 col-lg-3">
-                <Kategori />
-                <Insta />
-              </div>
+                    </div>
+                  </div>
+                  </Link>
+                ) : (
+                  <Link to={`/kabarterkini/${edge.node.slug}`} style={{textDecoration:'none'}}>
+                  <div
+                    className="card"
+                    style={{
+                      borderRadius: "45px",
+                      minHeight: "100px",
+                      marginBottom: "30px",
+                    }}
+                  >
+                    <img
+                      className="card-img-top"
+                      src={edge.node.gambarArtikel.file.url}
+                      style={{
+                        height: "20rem",
+                        objectFit: "cover",
+                        borderRadius: "45px 45px 0px 0px",
+                      }}
+                    ></img>
+                    <div className="card-body">
+                      <div className="container">
+                        <div className="row">
+                          <div className="col-sm-12 col-md-8 col-lg-10 text-left pl-3 pb-3">
+                            <h4 style={ {color:'#005131'}}>{edge.node.title}</h4>
+                          </div>
+
+                          <div className="col-sm-12 col-md-4 col-lg-2 pl-0 text-left mb-3">
+                            {edge.node.publishedDate}
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                  </div>           
+                  </Link>
+                      
+                )
+              })}
+              {/*          
+             <div
+                      className="card"
+                      style={{ minHeight: "100px", marginBottom: "30px" }}
+                    >
+                      <div className="card-body text-center">
+                        <li className={blogStyles.post}>
+                          <Link to={`/kabarterkini/${edge.node.slug}`}>
+                            <h2>{edge.node.title}</h2>
+                            <p>{edge.node.publishedDate}</p>
+                          </Link>
+                        </li>
+                      </div>
+                    </div> */}
+
+              <Pager pageContext={pageContext} />
             </div>
-          </Layout>
+            <div className="col-md-4 col-lg-3 text-left">
+              <Kategori />
+              <Insta />
+            </div>
+          </div>
+        </Layout>
         )
       }
 
